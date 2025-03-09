fc48ad0f195e147dbedd5cce76a59bb2ee38d48b
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2025-03-09 14:34:24 -0300

    improve switch comments

--- conditionals/switch.playground/Contents.swift
+++ conditionals/switch.playground/Contents.swift
@@ -1,8 +1,10 @@
 import UIKit
 
-// In case of multiple else ifs, switch is more adequate, because it's create to validate a series of possible cases.
+// When dealing with multiple conditions, a switch statement is more appropriate than multiple 'else if' statements
+// because it's specifically designed to evaluate multiple possible cases.
 
-// In swift, switches must validates every single option, often containing a default option.
+// In Swift, switch statements must be exhaustive, covering all possible values,
+// which typically requires including a default case.
 
 var number = 7
 switch number % 2 {
@@ -13,7 +15,7 @@ default:
 }
 
 
-// Multiple statements in the same case
+// Multiple values can be tested in a single case
 let letter = "z"
 switch letter {
 case "a", "e", "i", "o", "u":
@@ -23,7 +25,7 @@ default:
 }
 
 
-// Letter range
+// Using character ranges in switch cases
 switch letter {
 case "a"..."f":
     print("You're in class 1")
@@ -36,17 +38,17 @@ default:
 }
 
 
-// Number range
+// Using numeric ranges in switch cases
 let speed = 33.0
 switch speed {
 case 0.0..<20.0:
-    print("First march")
+    print("First gear")
 case 20.0..<40.0:
-    print("Second march")
+    print("Second gear")
 case 40.0..<50.0:
-    print("Third march")
+    print("Third gear")
 case 50.0..<90.0:
-    print("Fourth march")
+    print("Fourth gear")
 default:
-    print("Fifth march")
+    print("Fifth gear")
 }
